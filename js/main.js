f0725d46a5f1d12a803db35b9d926d42b4e6ebba
abdullahnettoor <abdullahnettoor@gmail.com>
2023-08-25 12:44:56 +0530

    Form validation fixed

--- js/main.js
+++ js/main.js
@@ -522,75 +522,75 @@ var jarallaxPlugin = function() {
 	});
 };
 
-// var contactForm = function() {
-// 	if ($('#contactForm').length > 0 ) {
-// 		$( "#contactForm" ).validate( {
-// 			rules: {
-// 				name: {
-// 					required: true, 
-// 					minlength: 3,
-// 					normalizer: function(value) {
-// 						return $.trim(value);
-// 					}
-// 				},
-// 				email: {
-// 					required: true,
-// 					email: true,
-// 				},
-// 				message: {
-// 					required: true,
-// 					minlength: 10,
-// 					normalizer: function(value) {
-// 						return $.trim(value);
-// 					}
-// 				}
-// 			},
-// 			messages: {
-// 				name: "Please enter your name",
-// 				email: "Please enter a valid email address",
-// 				message: "Please enter a message"
-// 			},
-// 			errorElement: 'span',
-// 			errorLabelContainer: '.form-error',
-// 			/* submit via ajax */
-// 			submitHandler: function(form) {		
-// 				var $submit = $('.submitting'),
-// 					waitText = 'Submitting...';
+var contactForm = function() {
+	if ($('#contactForm').length > 0 ) {
+		$( "#contactForm" ).validate( {
+			rules: {
+				name: {
+					required: true, 
+					minlength: 3,
+					normalizer: function(value) {
+						return $.trim(value);
+					}
+				},
+				email: {
+					required: true,
+					email: true,
+				},
+				message: {
+					required: true,
+					minlength: 10,
+					normalizer: function(value) {
+						return $.trim(value);
+					}
+				}
+			},
+			messages: {
+				name: "Please enter your name",
+				email: "Please enter a valid email address",
+				message: "Please enter a message"
+			},
+			errorElement: 'span',
+			errorLabelContainer: '.form-error',
+			/* submit via ajax */
+			// submitHandler: function(form) {		
+			// 	var $submit = $('.submitting'),
+			// 		waitText = 'Submitting...';
 
-// 				$.ajax({   	
-// 			      type: "POST",
-// 			      url: "php/send-email.php",
-// 			      data: $(form).serialize(),
+			// 	$.ajax({   	
+			//       type: "POST",
+			//       url: "https://api.web3forms.com/submit",
+			//       data: $(form).serialize(),
 
-// 			      beforeSend: function() { 
-// 			      	$submit.css('display', 'block').text(waitText);
-// 			      },
-// 			      success: function(msg) {
-// 	               if (msg == 'OK') {
-// 	               	$('#form-message-warning').hide();
-// 			            setTimeout(function(){
-// 	               		$('#contactForm').fadeOut();
-// 	               	}, 1000);
-// 			            setTimeout(function(){
-// 			               $('#form-message-success').fadeIn();   
-// 	               	}, 1400);
+			//       beforeSend: function() { 
+			//       	$submit.css('display', 'block').text(waitText);
+			//       },
+			//       success: function(msg) {
+	        //        if (msg == 'OK') {
+	        //        	$('#form-message-warning').hide();
+			//             setTimeout(function(){
+	        //        		$('#contactForm').fadeOut();
+	        //        	}, 1000);
+			//             setTimeout(function(){
+			//                $('#form-message-success').fadeIn();   
+	        //        	}, 1400);
 		               
-// 		            } else {
-// 		               $('#form-message-warning').html(msg);
-// 			            $('#form-message-warning').fadeIn();
-// 			            $submit.css('display', 'none');
-// 		            }
-// 			      },
-// 			      error: function() {
-// 			      	$('#form-message-warning').html("Something went wrong. Please try again.");
-// 			         $('#form-message-warning').fadeIn();
-// 			         $submit.css('display', 'none');
-// 			      }
-// 		      });    		
-// 	  		}
-// 		} );
-// 	}
-// };
+		    //         } else {
+		    //            $('#form-message-warning').html(msg);
+			//             $('#form-message-warning').fadeIn();
+			//             $submit.css('display', 'none');
+		    //         }
+			//       },
+			//       error: function() {
+			//       	$('#form-message-warning').html("Something went wrong. Please try again.");
+			//          $('#form-message-warning').fadeIn();
+			//          $submit.css('display', 'none');
+			//       }
+		    //   });    		
+	  		// }
+		} );
+	}
+};
 
 var stickyFillPlugin = function() {
 	var elements = document.querySelectorAll('.unslate_co--sticky');
